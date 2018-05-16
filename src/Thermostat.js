89e9d1362e7520f8af0dbd793353b76cac244ca6
Charly Mannion <charlottemannion100@gmail.com>
2018-05-16 13:00:39 +0100

    constants added to thermostat

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,13 +2,17 @@
 
 function Thermostat() {
   const defaultTemperature = 20
+  const defaultValue = 1
   this.temperature = defaultTemperature;
+  this.incrementValue = defaultValue;
 }
 
 Thermostat.prototype.increaseTemperature = function() {
-  this.temperature++
+  // this.temperature++
+  this.temperature = this.temperature + this.incrementValue
 }
 
 Thermostat.prototype.decreaseTemperature = function() {
-  this.temperature--
+  // this.temperature--
+  this.temperature = this.temperature - this.incrementValue
 }
