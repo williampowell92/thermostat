0a7b5e44cfe4c6eed1af91aedf55ee9713a5d7d3
William Powell <william.powell92@gmail.com>
2018-05-16 12:20:30 +0100

    Thermostat has default temp of 20, test for increaseTemp added

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,20 @@
+describe("Thermostat", function() {
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  describe("temperature", function() {
+    it("starts at 20", function() {
+      expect(thermostat.temperature).toEqual(20)
+    });
+  });
+
+  describe("increaseTemperature", function() {
+    it("increases the temperature by 1", function() {
+      thermostat.increaseTemperature();
+      expect(thermostat.temperature).toEqual(1);
+    });
+  });
+});
