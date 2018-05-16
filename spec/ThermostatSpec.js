bd34a64a6c31e1cefedbcad74f25bf0a2a5e721f
William Powell <william.powell92@gmail.com>
2018-05-16 16:35:43 +0100

    removed magic numbers in thermostat test

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -2,11 +2,7 @@
 
 describe("Thermostat", function() {
   var thermostat
-  const defaultTemperature = 20
   const incrementValue = 1
-  const minimumTemperature = 10
-  const powerSavingOnMaxTemp = 25
-  const powerSavingOffMaxTemp = 32
 
   beforeEach(function() {
     thermostat = new Thermostat()
@@ -14,18 +10,18 @@ describe("Thermostat", function() {
 
   describe("temperature", function() {
     it("starts at 20", function() {
-      expect(thermostat.temperature).toEqual(defaultTemperature)
+      expect(thermostat.temperature).toEqual(thermostat.DEFAULT_TEMPERATURE)
     });
   });
 
   describe("increaseTemperature", function() {
     it("increases the temperature by 1", function() {
       thermostat.increaseTemperature()
-      expect(thermostat.temperature).toEqual(defaultTemperature+incrementValue)
+      expect(thermostat.temperature).toEqual(thermostat.DEFAULT_TEMPERATURE + incrementValue)
     });
 
     it("cannot go above maximum temperature in power saving mode", function() {
-      thermostat.temperature = powerSavingOnMaxTemp
+      thermostat.temperature = thermostat.MAX_TEMP_PSM_ON
       expect(function() {
         thermostat.increaseTemperature()
       }).toThrowError("Maximum temperature reached")
@@ -33,7 +29,7 @@ describe("Thermostat", function() {
 
     it("can go above default max temp out of power saving mode", function() {
       thermostat.isPowerSaving = false
-      thermostat.temperature = powerSavingOnMaxTemp
+      thermostat.temperature = thermostat.MAX_TEMP_PSM_ON
       expect(function() {
         thermostat.increaseTemperature()
       }).not.toThrowError("Maximum temperature reached")
@@ -43,11 +39,11 @@ describe("Thermostat", function() {
   describe("decreaseTemperature", function() {
     it("decreases the temperature by 1", function() {
       thermostat.decreaseTemperature()
-      expect(thermostat.temperature).toEqual(defaultTemperature-incrementValue)
+      expect(thermostat.temperature).toEqual(thermostat.DEFAULT_TEMPERATURE - incrementValue)
     });
 
     it("cannot go below minimum temperature", function() {
-      thermostat.temperature = minimumTemperature
+      thermostat.temperature = thermostat.DEFAULT_MINIMUM_TEMPERATURE
       expect(function() {
         thermostat.decreaseTemperature()
       }).toThrowError("Minimum temperature reached")
@@ -75,38 +71,38 @@ describe("Thermostat", function() {
   describe("_maximumTemperature", function(){
     it("is 25 in power saving mode", function(){
       thermostat.isPowerSaving = true
-      expect(thermostat._maximumTemperature()).toEqual(25)
+      expect(thermostat._maximumTemperature()).toEqual(thermostat.MAX_TEMP_PSM_ON)
     });
     it("is 32 if power saving mode is off", function(){
       thermostat.isPowerSaving = false
-      expect(thermostat._maximumTemperature()).toEqual(32)
+      expect(thermostat._maximumTemperature()).toEqual(thermostat.MAX_TEMP_PSM_OFF)
     });
   });
 
   describe("resetTemperature", function() {
     it("resets the temperature to default", function() {
-      thermostat.temperature = powerSavingOnMaxTemp
+      thermostat.temperature = thermostat.MAX_TEMP_PSM_ON
       thermostat.resetTemperature()
-      expect(thermostat.temperature).toEqual(defaultTemperature)
+      expect(thermostat.temperature).toEqual(thermostat.DEFAULT_TEMPERATURE)
     });
   });
 
   describe("energyUsage", function() {
     it("is low when temperature is below 18", function(){
-      for (var temp = minimumTemperature; temp < 18; temp++) {
+      for (var temp = thermostat.DEFAULT_MINIMUM_TEMPERATURE; temp < thermostat.MEDIUM_ENERGY_USAGE_LIMIT; temp++) {
         thermostat.temperature = temp
         expect(thermostat.energyUsage()).toEqual("low-usage")
       };
     });
     it("is medium when the temperature is between 18 and 24", function (){
-      for (var temp = 18; temp < 25; temp++) {
+      for (var temp = thermostat.MEDIUM_ENERGY_USAGE_LIMIT; temp < thermostat.MAX_TEMP_PSM_ON; temp++) {
         thermostat.temperature = temp
         expect(thermostat.energyUsage()).toEqual("medium-usage")
       };
     });
     it("is high when the temperature is 25 or above", function(){
       thermostat.isPowerSaving = false
-      for (var temp = 25; temp < powerSavingOffMaxTemp; temp++) {
+      for (var temp = thermostat.MAX_TEMP_PSM_ON; temp < thermostat.MAX_TEMP_PSM_OFF; temp++) {
         thermostat.temperature = temp
         expect(thermostat.energyUsage()).toEqual("high-usage")
       };
