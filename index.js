224d8919c78c525488af8f7fe3c70b56859b2abf
William Powell <william.powell92@gmail.com>
2018-05-17 16:05:16 +0100

    Added jquery button functions

--- index.js
+++ index.js
@@ -1,16 +1,71 @@
-console.log(window)
+
 $(document).ready(function() {
-  $('#temperature-up').click(function(){
-    $("body").removeClass().addClass("high-usage");
-  });
+  var thermostat = new Thermostat
+  updateTemperature();
 
-  $('#temperature-down').click(function(){
+  function updateTemperature() {
+    $("#temperature").text(thermostat.temperature);
+  };
+
+  function lowUsage() {
     $("body").removeClass().addClass("low-usage")
-  });
+  }
 
-  $("body").addClass("low-usage")
-
-  $('#temperature-reset').click(function(){
+  function mediumUsage() {
     $("body").removeClass().addClass("medium-usage")
+  }
+
+  function highUsage() {
+    $("body").removeClass().addClass("high-usage")
+  }
+
+  function updateEnergyUsage() {
+    if (thermostat.energyUsage() === "low-usage") {
+      $("body").removeClass().addClass("low-usage")
+    } else if (thermostat.energyUsage() === "medium-usage") {
+      $("body").removeClass().addClass("medium-usage")
+    } else {
+      $("body").removeClass().addClass("high-usage");
+    }
+  };
+
+
+
+  $("#temperature-up").click(function() {
+    thermostat.increaseTemperature()
+    updateTemperature()
+    updateEnergyUsage()
   });
+
+  $("#temperature-down").click(function() {
+    thermostat.decreaseTemperature()
+    updateTemperature()
+    updateEnergyUsage()
+  });
+
+  $("#temperature-reset").click(function() {
+    thermostat.resetTemperature()
+    updateTemperature()
+    updateEnergyUsage()
+  })
+
+  $("#power-saving").click(function() {
+    thermostat.togglePowerSaving()
+    updateTemperature()
+    updateEnergyUsage()
+  })
+
+  // $('#temperature-up').click(function() {
+  //   $("body").removeClass().addClass("high-usage");
+  // });
+  //
+  // $('#temperature-down').click(function() {
+  //   $("body").removeClass().addClass("low-usage")
+  // });
+  //
+  // $('#temperature-reset').click(function() {
+  //   $("body").removeClass().addClass("medium-usage")
+  // });
+
+  // $("#temperature").html(thermostat.temperature);
 });
